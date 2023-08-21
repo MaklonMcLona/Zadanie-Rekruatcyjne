3e23b21b244c536b404417594a610c06d11e3584
MaklonMcLona <arkadisuz.barnak@gmail.com>
2023-08-21 14:55:57 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+const masonry = new Macy({
+    container: '#photoGallery',
+    mobileFirst: true,
+    columns: 1,
+    breakAt: {
+        800: 2,
+        1100: 3,
+    },
+    margin:{
+        x: 40,
+        y: 50,
+    }
+});
+
+
+        
